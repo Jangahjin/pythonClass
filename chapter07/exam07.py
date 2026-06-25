bb717731c71db22192b4e50762fe4cef06b7561a
Jangahjin <snita8379gmail.com>
2026-06-25 17:50:35 +0900

    python chapter07

---
+++ chapter07/exam07.py
@@ -0,0 +1,12 @@
+# Quiz) "리그 오브 레전드" 게임에서 세명의 팀원이 얻은 골드량을 매개변수로 
+# 받아, 팀의 평균 골드 획득량을 계산하는 함수 cal_average_gold를 정의하고 사
+# 용해 보세요. + 가변인자 받아서 실행하는 함수를 프로그램
+# cal_average_gold(12000, 15000, 18000)
+# 실행결과
+# 15000.0
+
+def cal_average_gold(gold1, gold2, gold3):
+num = gold1 + gold2 + gold3
+return num / 3
+
+def  cal_average_gold(12000, 15000, 18000)
